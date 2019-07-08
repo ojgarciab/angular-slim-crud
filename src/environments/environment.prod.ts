142e2053da77f541946eebcaf63edeef0ee87b08
Oscar García <oscar.github@linaresdigital.com>
2019-07-08 12:59:24 +0200

    Cambios para no exponer clientId

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
+import { AuthConfig } from 'angular-oauth2-oidc';
+
+export const authConfig: AuthConfig = {
+  // Test with google oauth2
+  issuer: 'https://accounts.google.com',
+  redirectUri: window.location.origin + '/web/inicio',
+  silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html',
+  clientId: '<Id de cliente público>',
+  strictDiscoveryDocumentValidation: false,
+  scope: 'openid profile email',
+  showDebugInformation: true,
+  sessionChecksEnabled: true,
+};
+
 export const environment = {
   production: true
 };
